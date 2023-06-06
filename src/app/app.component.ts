aca27c5bf106f63fdc298fbdcdc1524ee15b437c
Thiago Sato <thiago.sato@shift.com.br>
2023-06-06 13:40:29 -0300

    Primeira Aula

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {
+  ModalRef,
+  ModalService,
+} from './shared/components/modal/services/modal.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  @ViewChild('modal')
+  public modalTemplateRef: TemplateRef<any>;
+  title = 'a11y-p2';
+  public firstName = 'Thiago';
+  public modalRef: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+  public show(): void {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details',
+    });
+  }
+}
