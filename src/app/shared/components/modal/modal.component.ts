ba5c4306c68ad71786718e9e83a1a7ff275c8b4c
Thiago Sato <thiago.sato@shift.com.br>
2023-06-06 15:59:30 -0300

    Segunda Aula

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { ModalConfig } from './interfaces/modal-config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['modal.component.scss'],
 })
-export class ModalComponent {}
+export class ModalComponent {
+  public config: ModalConfig;
+}
