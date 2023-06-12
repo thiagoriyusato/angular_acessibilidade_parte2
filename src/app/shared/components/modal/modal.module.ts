b28d9bbf0c8727347d89a9781f13102737f3320a
Thiago Sato <thiago.sato@shift.com.br>
2023-06-12 11:58:06 -0300

    Quarta aula

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
+import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FocusTrapModule],
   exports: [ModalComponent],
   providers: [ModalService],
 })
